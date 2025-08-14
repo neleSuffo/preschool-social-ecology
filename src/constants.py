79d8ad6ccb87fc791876621938b386de52459992
NeleSuffo <nele.suffo@leuphana.de>
2025-08-14 12:27:46 +0200

    Add SUBJECTS_CSV_PATH and INFERENCE_DB_PATH to DataPaths class

--- src/constants.py
+++ src/constants.py
@@ -21,6 +21,8 @@ class DataPaths:
     ANNO_DB_PATH = Path(ANNO_DIR/"quantex_annotations.db")
     RAWFRAMES_EXTRACTION_ERROR_LOG = Path(BasePaths.DATA_DIR/"rawframes_extraction_error.log")
     PROCESSED_VIDEOS_LOG = Path(BasePaths.DATA_DIR/"processed_videos.log")
+    SUBJECTS_CSV_PATH = Path(BasePaths.DATA_DIR/"quantex_subjects.csv")
+    INFERENCE_DB_PATH = Path(BasePaths.OUTPUT_DIR/"quantex_inference/inference_database.db")
 
 class AudioClassification:
     AUDIO_FILES_DIR = Path(BasePaths.DATA_DIR/"childlens_audio")
