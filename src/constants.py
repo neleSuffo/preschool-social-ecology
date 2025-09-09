f623c27b895116f88f66540f38218fe18a4a3e61
neleSuffo <163427736+neleSuffo@users.noreply.github.com>
2025-09-09 14:01:32 +0200

    feat: update turn taking CSV filename and add interaction composition CSV path

--- src/constants.py
+++ src/constants.py
@@ -84,6 +84,7 @@ class Inference:
     INTERACTION_SEGMENTS_CSV = BASE_OUTPUT_DIR / "interaction_segments.csv"
     UTTERANCE_SEGMENTS_CSV = BASE_OUTPUT_DIR / "utterance_segments.csv"
     KCS_SUMMARY_CSV = BASE_OUTPUT_DIR / "kcs_summary.csv"
-    TURN_TAKING_CSV = BASE_OUTPUT_DIR / "turn_taking.csv"
+    TURN_TAKING_CSV = BASE_OUTPUT_DIR / "turn_taking_summary.csv"
     CDS_SUMMARY_CSV = BASE_OUTPUT_DIR / "cds_summary.csv"
     PRESENCE_COUNTS_CSV = BASE_OUTPUT_DIR / "presence_counts.csv"
+    INTERACTION_COMPOSITION_CSV = BASE_OUTPUT_DIR / "interaction_composition.csv"
