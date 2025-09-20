884146b21ddbf07d7aad9e0a1c5a1810bfcc5e33
NeleSuffo <nele.suffo@leuphana.de>
2025-09-20 23:20:24 +0200

    refactor: simplify model loading and remove unused variables in audio classifier script

--- src/models/speech_type/run_audio_classifier.py
+++ src/models/speech_type/run_audio_classifier.py
@@ -1,25 +1,18 @@
 import argparse
-from pathlib import Path
 from constants import AudioClassification
-from utils import load_thresholds, load_inference_model, process_audio_folder
+from utils import load_thresholds, load_model, process_audio_folder
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run audio classifier on a folder of audio files.")
     parser.add_argument('--audio_folder', type=str, required=True, help="Path to folder containing WAV audio files.")
     args = parser.parse_args()
     
-    # Load the trained model and MultiLabelBinarizer once
-    results_dir = Path(AudioClassification.RESULTS_DIR)
-    model_path = results_dir / "best_model.keras"
-    
-    model, mlb = load_inference_model(model_path)
+    model, mlb = load_model()
     if model is None:
         exit()
     
     # Load optimized thresholds from training
-    thresholds = load_thresholds(results_dir, mlb.classes_)
+    thresholds = load_thresholds(mlb.classes_)
     
-    output_dir = AudioClassification.OUTPUT_DIR
-
     # Process all audio files in the specified folder and save results
-    process_audio_folder(args.audio_folder, model, mlb, output_dir, thresholds)
+    process_audio_folder(args.audio_folder, model, mlb, thresholds)
